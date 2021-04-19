a3fae8d1ff8fd5ab02d8a03a7761135a6e923006
MinG822 <lucida822@naver.com>
2021-04-19 09:17:46 +0900

    modify inheritance package

--- src/inheritance/CustomerTest.java
+++ src/inheritance/CustomerTest.java
@@ -2,23 +2,36 @@ package inheritance;
 /*
 inheritance 패키지의 CustomerTest 클래스
 Customer 를 테스트한다.
+
+VIPCustomer 의 showSuperHash 와 showHash 가 같은데
+하위 클래스에서 super를 통해 상위클래스에 접근할 때
+힙메모리에서 하위클래스의 영역 중 상위 클래스의 부분을 접근한다는 게 정확할 것 같다.
+
+하위클래스를 생성하며 상위클래스 형으로 선언할 때는
+하위클래스 생성자가 호출되기 전에 상위 클래스 생성자가 호출되는 것까지 같은데
+만들어진 하위클래스의 멤버변수나 메서드에 접근할 수 없게 된다.
+
 */
 public class CustomerTest {
 	
 	public static void main(String[] args) {
-		Customer mong = new Customer();
-		mong.setCustomerId(1010);
-		mong.setCustomerName("mong");
+		Customer mong = new Customer(1010, "mong");
 		mong.bonusPoint = 1000; // 보너스 포인트를 protected 로 설정해야하지 않나
 		System.out.println(mong.showCustomerInfo());
 		
 		
-		VIPCustomer vmong = new VIPCustomer();
-		vmong.setCustomerId(1020);
-		vmong.setCustomerName("vip mong");
+		VIPCustomer vmong = new VIPCustomer(1010, "vip mong", 1004);
 		vmong.bonusPoint = 10000;
-		System.out.println(vmong.showCustomerInfo());
+		System.out.println(vmong.showVIPInfo());
 		
+		mong.showHash();
+		vmong.showSuperHash();
+		vmong.showHash();
+		
+		Customer vvmong = new VIPCustomer(1011, "vvip mong", 1004); // 업캐스팅
+		vvmong.showCustomerInfo();
+//		vvmong.showSuperHash(); 참조할 수 없다
+//		VIPCustomer mmong = new Customer(1011, "mmong"); 상위클래스를 생성하며 하위클래스 형으로 지정할 수 없다.
 	}
 
 }
--- src/inheritance/VIPCustomer.java
+++ src/inheritance/VIPCustomer.java
@@ -5,19 +5,41 @@ inheritance 패키지의 VIP Customer 클래스
 Customer 클래스를 상속받고 VIP Customer 에게만 필요한 추가 속성과 기능을 구현한다.
 상속은 extends 예약어를 사용한다.
 
+VIPCustomer 생성자가 호출되기 전에 Customer() 생성자가 먼저 호출된다.
+
+하위 클래스의 인스턴스가 생성될 때 
+상위 클래스의 생성자가 호출되어 (컴파일러가 자동으로 super()를 추가한다) 
+상위 클래스의 멤버 변수(private 포함)가 메모리에 생성되고나서
+하위 클래스의 멤버 변수가 메모리에 생성된다.
+그래서 하위 클래스에서도 이 값들을 public 과 protected 인 경우 모두 사용할 수 있음 
+
+super 예약어는 하위 클래스에서 상위 클래스로 접근할 때 사용함
+super 는 상위클래스의 주소(참조 값)를 가지고 있음
 */
 public class VIPCustomer extends Customer {
-	private int agentID;
+	private int agentId;
 	double saleRatio;
 	
-	public VIPCustomer() {
+	public VIPCustomer(int customerId, String customerName, int agentId) {
+		super(customerId, customerName); // 컴파일러가 ㅈ동으로 추가하는 코드, 상위클래스의 Customer() 가 호출됨
 		customerGrade = "VIP";
 		bonusRatio = 0.05;
 		saleRatio = 0.1;
+		this.agentId = agentId;
+		System.out.println("VIPCustomer() 생성자 호출");
 	}
 	
 	public int getAgentId() {
-		return agentID;
+		return agentId;
+	}
+	
+	public String showVIPInfo() {
+		
+		return super.showCustomerInfo() + ". 담당 상담원 아이디는" + agentId + "입니다";
+	}
+	
+	public void showSuperHash() {
+		System.out.println(super.hashCode());
 	}
 	
 }
